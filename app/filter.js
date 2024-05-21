9927efcb9787c45df93a4219c687f242f6128213
João Vitor Bagatoli <joaovitorbagatoli07@gmail.com>
2024-05-21 16:16:39 -0300

    new

--- app/filter.js
+++ app/filter.js
@@ -5,6 +5,6 @@ btns.forEach(btn => btn.addEventListener('click', filtrarLivros))
 function filtrarLivros() {
     const btn = document.getElementById(this.id)
     const categoria = btn.value
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
+    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
 }
--- app/forEach.js
+++ app/forEach.js
@@ -3,10 +3,11 @@ const elementoParaInserirLivros = document.getElementById('livros')
 function exibirOsLivrosNaTela(listaDeLivros){
     elementoParaInserirLivros.innerHTML = ''
     listaDeLivros.forEach(livro => {
+        let disponibilidade = livro.quantidade <= 0 ? 'indisponivel' : ''
         elementoParaInserirLivros.innerHTML += 
         `
         <div class="livro">
-            <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+            <img class="livro__imagens ${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
             <h2 class="livro__titulo">${livro.titulo}</h2>
             <p class="livro__descricao">${livro.autor}</p>
             <p class="livro__preco" id="preco">${livro.preco.toFixed(2)}</p>
